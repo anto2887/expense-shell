44fb7f5246fd58dd2372c6ded853d79e0abae7b9
uktony <uktony12@gmail.com>
2023-11-02 19:13:30 -0500

    new edit

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,28 @@
-dnf install nginx -y
+log_file=/tmp/expense.log
+color="\e[34m"
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo -e "${color} Install Nginx \e[0m"
+dnf install nginx -y &>>$log_file
+echo $?
 
-rm -rf /usr/share/nginx/html/*
+echo -e "${color} Copy in the expense.conf file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "${color} Remove the default content that web server is serving \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+echo -e "${color} Download the frontend content \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
 
-systemctl enable nginx 
-systemctl start nginx 
+echo -e "${color} Download the frontend content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+echo $?
+
+echo -e "${color} Start & Enable Nginx service \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>$log_file
+echo $?
