875e12bd262db5f593030c10079f65636e6fd579
uktony <uktony12@gmail.com>
2023-11-03 14:56:09 -0500

    changed the ip addresses

--- backend.sh
+++ backend.sh
@@ -2,44 +2,44 @@ log_file=/tmp/expense.log
 color="\e[35m"
 
 echo -e "${color} Disable default NodeJS version and enable NodeJS v18 \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:18 -y &>>$log_file
 echo $?
 
 echo -e "${color} Add in the backend service \e[0m"
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 echo $?
 
 echo -e "${color} Install NodeJS v18 \e[0m"
-dnf install nodejs -y
+dnf install nodejs -y &>>$log_file
 echo $?
 
 echo -e "${color} Add Expense User \e[0m"
-useradd expense
+useradd expense &>>$log_file
 echo $?
 
 echo -e "${color} Create the app dir \e[0m"
-mkdir /app
+mkdir /app &>>$log_file
 echo $?
 
 echo -e "${color} Download the application code to the app directory \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
-cd /app 
-unzip /tmp/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+cd /app &>>$log_file
+unzip /tmp/backend.zip &>>$log_file
 echo $?
 
 echo -e "${color} Create the dependencies within the app directory \e[0m"
-cd /app 
-npm install
+cd /app &>>$log_file
+npm install &>>$log_file
 echo $?
 
 echo -e "${color} Install mysql and load the schema to the db \e[0m"
-dnf install mysql -y
-mysql -h 172.31.23.112 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+dnf install mysql -y &>>$log_file
+mysql -h 172.31.23.112 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 echo $?
 
 echo -e "${color} Reload the daemon service, enable and start the backend service \e[0m"
-systemctl daemon-reload
-systemctl enable backend 
-systemctl start backend
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 echo $?
--- mysql.sh
+++ mysql.sh
@@ -2,22 +2,22 @@ log_file=/tmp/expense.log
 color="\e[31m"
 
 echo -e "${color} Disable MySQL 8 \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$log_file
 echo $?
 
 echo -e "${color} Copy in the mysql.repo file \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 echo $?
 
 echo -e "${color} Install MySQL Server \e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>$log_file
 echo $?
 
 echo -e "${color} Start MySQL Service \e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 echo $?
 
 echo -e "${color} Changing the default root database password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
 echo $?
