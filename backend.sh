641d5b0659a9ee5133e1069709610327b7a4ce2e
uktony <uktony12@gmail.com>
2023-11-03 14:16:38 -0500

    made changes to backend.sh and mysql.sh

--- backend.sh
+++ backend.sh
@@ -1,24 +1,45 @@
+log_file=/tmp/expense.log
+color="\e[35m"
+
+echo -e "${color} Disable default NodeJS version and enable NodeJS v18 \e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+echo $?
 
+echo -e "${color} Add in the backend service \e[0m"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 
+echo -e "${color} Install NodeJS v18 \e[0m"
 dnf install nodejs -y
+echo $?
 
+echo -e "${color} Add Expense User \e[0m"
 useradd expense
+echo $?
 
+echo -e "${color} Create the app dir \e[0m"
 mkdir /app
+echo $?
 
+echo -e "${color} Download the application code to the app directory \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
 cd /app 
 unzip /tmp/backend.zip
+echo $?
 
+echo -e "${color} Create the dependencies within the app directory \e[0m"
 cd /app 
-npm install 
+npm install
+echo $?
 
+echo -e "${color} Install mysql and load the schema to the db \e[0m"
 dnf install mysql -y
 mysql -h 172.31.17.54 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo $?
 
+echo -e "${color} Reload the daemon service, enable and start the backend service \e[0m"
 systemctl daemon-reload
 systemctl enable backend 
 systemctl start backend
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,23 @@
-dnf module disable mysql -y 
+log_file=/tmp/expense.log
+color="\e[31m"
 
+echo -e "${color} Disable MySQL 8 \e[0m"
+dnf module disable mysql -y
+echo $?
+
+echo -e "${color} Copy in the mysql.repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo $?
 
+echo -e "${color} Install MySQL Server \e[0m"
 dnf install mysql-community-server -y
+echo $?
 
+echo -e "${color} Start MySQL Service \e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+echo $?
 
+echo -e "${color} Changing the default root database password \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1
+echo $?
